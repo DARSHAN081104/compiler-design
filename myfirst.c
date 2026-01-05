14fa5f4a75ec717bbfd51ca452da31d0bb395870
Darshan <darshankyada2005@gmail.com>
2026-01-05 10:05:45 +0530

    Adding

---
+++ myfirst.c
@@ -0,0 +1,28 @@
+// #include<stdio.h>
+// int main() {
+//     int a,b,c=0;
+//     printf("Enter two numbers:");
+//     scanf("%d %d",&a,&b);
+//     c=a+b;
+//     printf("Sum is %d\n",c);
+
+// int number;
+// printf("Enter an integer: ");
+// scanf("%d", &number);
+// if(number % 2 == 0) {
+//     printf("%d is even.\n", number);
+// } else {
+//     printf("%d is odd.\n", number);
+//   }
+
+// int a,b;
+// printf("Enter two numbers: ");
+// scanf("%d %d",&a,&b);
+//   if(a>b){
+//     printf("%d is the largest number.\n",a);
+//   }else if(b>a){
+//     printf("%d is the largest number.\n",b);
+//   }else{
+//     printf("Both numbers are equal.\n");
+//   }
+// }
